603e9c852b442c143fe209c80465864da7eaea2f
Alyssa DiCarlo <alyssa.dicarlo@gmail.com>
2021-06-15 13:29:04 -0700

    added dns lookup exercise

---
+++ dns_lookup.js
@@ -0,0 +1,17 @@
+const dns = require('dns');
+const readline = require('readline');
+
+const input = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+input.question("Domain Name: ", (domain) => {
+    dns.lookup(domain, (error, address) => {
+        if (error !== null) {
+            console.log(error);
+        }
+        console.log(address);
+    });
+    input.close();
+});
